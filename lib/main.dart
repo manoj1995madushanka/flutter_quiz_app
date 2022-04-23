8e6f944907959c48c471246bc6b18301824d9f2a
manoj1995madushanka <madushankamanoj414@gmail.com>
2022-04-23 11:02:00 +0530

    dart function

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
 
+void addNumbers(int a,int b){
+  print("sum = ${a+b}");
+}
+
 void main() {
   for(int i=0;i<5;i++){
     print('hello ${i+1}');
   }
+  addNumbers(2, 2);
 }
